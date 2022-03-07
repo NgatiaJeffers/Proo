97b630a9c67f69c263b47722c3e318d5686dd609
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-03-07 18:06:41 +0300

    first commit

---
+++ primer/src/App.js
@@ -0,0 +1,13 @@
+import React,{ Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <h4 className='bg-primary text-white text-center p-2 m-1'>
+        Component Element
+      </h4>
+    );
+  }
+}
+
+export default App;
