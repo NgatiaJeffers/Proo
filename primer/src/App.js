fd686e252d2ffd5fa81b25240adad52e2809fdc1
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-03-07 18:35:53 +0300

    Learning

--- primer/src/App.js
+++ primer/src/App.js
@@ -1,10 +1,22 @@
 import React,{ Component } from 'react';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 4,
+    }
+  }
+
+  isEven(val) {
+    return val % 2 === 0 ? 'Even' : 'Odd';
+  }
+
   render() {
     return (
       <h4 className='bg-primary text-white text-center p-2 m-1'>
-        Component Element
+        Number of things: {this.isEven(this.state.count)}
       </h4>
     );
   }
