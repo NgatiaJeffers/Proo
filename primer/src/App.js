d2a821dceacd087327c1b0850fdda3eaefa1ecb0
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-03-07 18:49:07 +0300

    Using expressions to handle events

--- primer/src/App.js
+++ primer/src/App.js
@@ -13,9 +13,23 @@ class App extends Component {
     return val % 2 === 0 ? 'Even' : 'Odd';
   }
 
+  getClassName(val) {
+    return val % 2 === 0
+      ? 'bg-primary text-white text-center p-2 m-1'
+      : 'bg-secondary text-white text-center p-2 m-1';
+  }
+
+  handleClick = () => this.setState({ count: this.state.count + 1 });
+
   render() {
     return (
-      <h4 className='bg-primary text-white text-center p-2 m-1'>
+      <h4 className={this.getClassName(this.state.count)}>
+        <button
+          className="btn btn-info m-2"
+          onClick={this.handleClick}
+        >
+          Click Me  
+        </button>
         Number of things: {this.isEven(this.state.count)}
       </h4>
     );
