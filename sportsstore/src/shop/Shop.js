4ea12d4f622a589ebce7d5e0e4a65e8869b132f9
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-03-09 22:24:15 +0300

    Creating shopping features

---
+++ sportsstore/src/shop/Shop.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import CategoryNavigation from "./CategoryNavigation";
+import ProductList from "./ProductList";
+
+class Shop extends Component {
+    render () {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col bg-dark text-white">
+                        <div className="navbar-brand">SPORTS STORE</div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-3 p-2">
+                        <CategoryNavigation
+                            baseUrl="/shop/products"
+                            categories={this.props.categories}
+                        />
+                    </div>
+                    <div className="col-9 p-2">
+                        <ProductList 
+                            products={this.props.products}
+                        />
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default Shop;
