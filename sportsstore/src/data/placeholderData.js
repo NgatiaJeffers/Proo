64d819bad121aeb81373734a2f52adca5c4dac09
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-03-09 21:19:03 +0300

    Creating data store

---
+++ sportsstore/src/data/placeholderData.js
@@ -0,0 +1,40 @@
+export const data = {
+    categories: ["Watersports", "Soccer", "Chess", "Running"],
+    products: [
+        {
+            id: 1,
+            name: "P1",
+            category: "Watersports",
+            description: "Product 1 (Watersports)",
+            price: 275
+        },
+        {
+            id: 2,
+            name: "P2",
+            category: "Watersports",
+            description: "Product 2 (Watersports)",
+            price: 48.95
+        },
+        {
+            id: 3,
+            name: "P3",
+            category: "Running",
+            description: "Product 3 (Running)",
+            price: 18.95
+        },
+        {
+            id: 4,
+            name: "P4",
+            category: "Chess",
+            description: "Product 4 (Chess)",
+            price: 69.95
+        },
+        {
+            id: 5,
+            name: "P5",
+            category: "Chess",
+            description: "Product 5 (Chess)",
+            price: 29.95
+        },
+    ]
+}
