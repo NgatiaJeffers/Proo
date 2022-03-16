af331591524d9fc3d475667de390284b71066726
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-03-16 17:07:12 +0300

    Add card reducer

---
+++ sportsstore/src/data/CartReducer.js
@@ -0,0 +1,45 @@
+import { ActionTypes } from "./Types";
+
+export const CartReducer = (storeData, action) => {
+    let newStore = { cart: [], cartItems: 0, cartTotal: 0, ...storeData };
+    switch(action.type) {
+        case ActionTypes.CART_ADD:
+            const product = action.payload.product;
+            const quantity = action.payload.quantity;
+
+            let existing = newStore.cart.find(item => item.product.id === product.id);
+            if (existing) {
+                existing.quantity += quantity;
+            } else {
+                newStore.cart = [...newStore.cart, action.payload];
+            }
+            newStore.cartItems += quantity;
+            newStore.cartTotal += product.price * quantity;
+            return newStore;
+        
+        case ActionTypes.CART_UPDATE:
+            newStore.cart = newStore.cart.map(item => {
+                if (item.product.id === action.payload.product.id) {
+                    const diff = action.payload.quantity - item.quantity;
+                    newStore.cartItems += diff;
+                    newStore.cartTotal += (diff * item.product.price);
+                    return action.payload;
+                } else {
+                    return item;
+                }
+            });
+            return newStore;
+        case ActionTypes.CART_REMOVE:
+            let selection = newStore.cart.find(item => item.product.id === action.payload.product.id);
+            newStore.cartItems -= selection.quantity;
+            newStore.cartTotal -= (selection.quantity * selection.product.price);
+            newStore.cart = newStore.cart.filter(item => item.product.id !== action.payload.product.id);
+            return newStore;
+        
+        case ActionTypes.CART_CLEAR:
+            return { cart: [], cartItems: 0, cartTotal: 0 ...storeData };
+        
+        default:
+            return storeData || { cart: [], cartItems: 0, cartTotal: 0 };
+        }
+}
