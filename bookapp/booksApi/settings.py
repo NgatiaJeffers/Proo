2c68427a805171487bff4c99645dba64688e80f9
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-03-26 20:42:12 +0300

    Folder updates

--- bookapp/booksApi/settings.py
+++ bookapp/booksApi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-shbwbida_(tn@ed^-uzqqrrvrc%i3^s$pz^h4f&v6r(2ep8)+='
+SECRET_KEY = 'django-insecure-ma=r24512p#_5ta5d3t!clf5w1fb4!o!4%0#+4deo!$9eylh!2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
