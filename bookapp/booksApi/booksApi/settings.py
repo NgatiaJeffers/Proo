4a5226a9e07267f3bbfd653ecd109db891a442cf
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-03-26 21:14:44 +0300

    Set up Api

--- bookapp/booksApi/booksApi/settings.py
+++ bookapp/booksApi/booksApi/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# CORS settings
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 # Application definition
 
@@ -40,11 +43,13 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'books',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
