b50cf87cd7fae61cd3ef9c349582a9e17515b771
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-03-31 22:46:34 +0300

    Set up serializers

--- DjangoRest/restful01/toys/models.py
+++ DjangoRest/restful01/toys/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    release_date = models.DateField()
+    toy_category = models.CharField(max_length=50)
+    was_included_in_home = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('name',)
+    
+    def __str__(self):
+        return self.name
