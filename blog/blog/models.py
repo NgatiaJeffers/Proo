243d8df132762da77245d15b22cf23af0464ddf6
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-04-08 16:41:08 +0300

    Retrieve all data

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -2,7 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
 class Post(models.Model):
     STATUS_CHOISES = (
         ('draft', 'Draft'),
@@ -23,3 +27,6 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+
+    objects = models.Manager()
+    published = PublishedManager()
