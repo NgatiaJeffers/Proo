12a07c9d8f983b6d078cefa3d21f74e5a59ea2e6
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-05-18 05:02:47 +0300

    Add Files

---
+++ Node/confetti_cuisine/contentTypes.js
@@ -0,0 +1,17 @@
+export const contentTypes = {
+    html: {
+        "Content-Type": "text/html"
+    },
+    text: {
+        "Content-Type": "text/plain"
+    },
+    js: {
+        "Content-Type": "text/javascript"
+    },
+    jpg: {
+        "Content-Type": "image/jpeg"
+    },
+    css: {
+        "Content-Type": "text/css"
+    }
+};
---
+++ Node/confetti_cuisine/router.js
@@ -0,0 +1,24 @@
+import { StatusCodes } from "http-status-codes";
+import { contentTypes } from "./contentTypes";
+
+routes = {
+    "GET": {},
+    "POST": {}
+};
+
+export function handle (req, res) {
+    try {
+        routes(req.method)[req.url](req, res);
+    } catch (e) {
+        res.writeHead(StatusCodes.OK, contentTypes.html);
+        utils.getFile("views/error.html", res);
+    }
+};
+
+export function get (url, action) {
+    routes["GET"][url] = action;
+};
+
+export function post (url, action) {
+    routes["POST"][url] = action;
+};
