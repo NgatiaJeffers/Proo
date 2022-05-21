b2ed0f4ea1b07f11bb7652c7dd1891dd8b20c3aa
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-05-21 21:13:14 +0300

    Processing URL_encoded and JSON params

--- Node/confetti_cuisine/index.js
+++ Node/confetti_cuisine/index.js
@@ -1,12 +1,17 @@
 import * as http from 'http';
 import {StatusCodes} from 'http-status-codes';
-import * as express from 'express';
+import express from 'express';
 
 // Instantiate the express application
 const app = express();
 
 // set the port of our application
 app.set("port", process.env.PORT || 3000);
+app.use(express.urlencoded({
+    extended: false
+})); // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+
 
 // Add a route for the root path
 app.get("/", (req, res) => {
@@ -14,6 +19,6 @@ app.get("/", (req, res) => {
 });
 
 // Set the application up to listen on port 3000
-app.listen(app.get("post"), () => {
+app.listen(app.get("port"), () => {
     console.log(`Serving Confetti Cuisine on port ${app.get("port")}`);
 });
