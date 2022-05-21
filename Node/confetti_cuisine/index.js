81545251af50f3ff8f39a5d41b0693bf1b2488a5
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-05-21 21:06:35 +0300

    Set up routes for the application

--- Node/confetti_cuisine/index.js
+++ Node/confetti_cuisine/index.js
@@ -1,64 +1,19 @@
 import * as http from 'http';
 import {StatusCodes} from 'http-status-codes';
-import * as contentType from "./contentTypes.js";
-import {get, post, handleRequest} from './router';
-import { getFile } from './utils';
+import * as express from 'express';
 
-const port = process.env.PORT || 3000;
+// Instantiate the express application
+const app = express();
 
+// set the port of our application
+app.set("port", process.env.PORT || 3000);
 
-get("/", (req, res) => {
-    res.writeHead(StatusCodes.OK, contentType.html);
-    getFile("views/index.html", res);
+// Add a route for the root path
+app.get("/", (req, res) => {
+    res.send("Welcome to the Confetti Cuisine!");
 });
 
-get("/courses.html", (req, res) => {
-    res.writeHead(StatusCodes.OK, contentType.html);
-    getFile("views/courses.html", res);
+// Set the application up to listen on port 3000
+app.listen(app.get("post"), () => {
+    console.log(`Serving Confetti Cuisine on port ${app.get("port")}`);
 });
-
-get("/contact.html", (req, res) => {
-    res.writeHead(StatusCodes.OK, contentType.html);
-    getFile("views/contact.html", res);
-});
-
-post("/", (req, res) => {
-    res.writeHead(StatusCodes.OK, contentType.html);
-    getFile("views/thanks.html", res);
-});
-
-get("/graph.png", (req, res) => {
-    res.writeHead(StatusCodes.OK, contentType.png);
-    getFile("pubic/images/graph.png", res);
-});
-
-get("/people.jpg", (req, res) => {
-    res.writeHead(StatusCodes.OK, contentType.jpg);
-    getFile("public/images/people.jpg", res);
-});
-
-get("/product.jpg", (req, res) => {
-    res.writeHead(StatusCodes.OK, contentType.jpg);
-    getFile("public/images/product.jpg", res);
-})
-
-get("/styles.css", (req, res) => {
-    res.writeHead(StatusCodes.OK, contentType.css);
-    getFile("public/css/styles.css", res);
-});
-
-get("/bootstrap.min.css", (req, res) => {
-    res.writeHead(StatusCodes.OK, contentType.css);
-    getFile("public/css/bootstrap.min.css", res);
-});
-
-get("/confettiCuisine.js", (req, res) => {
-    res.writeHead(StatusCodes.OK, contentType.js);
-    getFile("public/js/confettiCuisine.js", res);
-});
-
-http.createServer((req, res) => {
-    handleRequest(req, res);
-}).listen(port);
-
-console.log(`Server running on port ${port}`);
