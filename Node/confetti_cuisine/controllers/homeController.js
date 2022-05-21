661d7cb53629e012605ec890a6e5e36fb39c5152
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-05-21 21:35:04 +0300

    Set up layouts and pass data for the courses

--- Node/confetti_cuisine/controllers/homeController.js
+++ Node/confetti_cuisine/controllers/homeController.js
@@ -1,6 +1,36 @@
+let courses = [
+    {
+        name: "How to cook the best burger pt. 4",
+        topic: "Javascript",
+        level: "Intermediate",
+        views: 0,
+        date: new Date(2019, 11, 1),
+        cost: 50
+    },
+    {
+        name: "How to cook the best burger pt. 3",
+        topic: "Javascript",
+        level: "Intermediate",
+        views: 4,
+        date: new Date(2020, 11, 1),
+        cost: 25
+    },
+    {
+        name: "How to cook the best burger pt. 2",
+        topic: "Javascript",
+        level: "Intermediate",
+        views: 2,
+        date: new Date(2020, 11, 1),
+        cost: 10
+    }
+]
+
+
 // Add callback functions for specific routes
 export function showCourses(req, res) {
-    res.render("Show all courses");
+    res.render("All courses", {
+        offeredCourses: courses
+    });
 };
 
 export function showSignUp(req, res) {
--- Node/confetti_cuisine/index.js
+++ Node/confetti_cuisine/index.js
@@ -1,17 +1,21 @@
 import * as http from 'http';
 import {StatusCodes} from 'http-status-codes';
 import express from 'express';
+import layoutes from 'express-ejs-layouts';
 import { showCourses, showSignUp, postedSignUp } from './controllers/homeController.js';
 
 // Instantiate the express application
 const app = express();
 
 // set the port of our application
+app.set("view engine", "ejs");  // set up ejs for templating
 app.set("port", process.env.PORT || 3000);
-app.use(express.urlencoded({
-    extended: false
-})); // for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: false})); // for parsing application/x-www-form-urlencoded
 app.use(express.json()); // for parsing application/json
+app.use(layoutes); // Set the app to use the layout engine
+
+// Enable static assets
+app.use(express.static("public"));
 
 
 // Add a route for the root path
