dda7c16257432355056f9d0f44f0b88107aaff45
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-05-27 18:16:45 +0300

    #Error handling on the app

---
+++ Node/recipeMongoApp/controllers/errorController.js
@@ -0,0 +1,19 @@
+import {StatusCodes} from "http-status-codes";
+
+export const logErrors = (err, req, res, next) => {
+    console.err(err.stack);
+    next(err);
+};
+
+export const pageNotFoundError = (req, res, next) => {
+    let errorCode = StatusCodes.NOT_FOUND;
+    res.status(errorCode);
+    res.render("error";)
+};
+
+export const internalServerError = (err, req, res, next) => {
+    let errorCode = StatusCodes.INTERNAL_SERVER_ERROR;
+    console.log(`ERROR occurred: ${err.stack}`);
+    res.status(errorCode);
+    res.render(`${errorCode} | Sorry, our application is experiencing a problem!`);
+};
